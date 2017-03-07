a807c5ff61d8a06b59d19fe355bc647d64466bad
rmichal2 <rmichal2@illinois.edu>
2017-03-06 23:31:03 -0600

    fixing comment

--- featureExtractor.py
+++ featureExtractor.py
@@ -7,9 +7,7 @@ This is a temporary script file.
 from readInInitData import readInInitData
 from readInGPSPoints import readInGPSPoints
 from computeBinaryFeatures import computeBinaryFeatures
-#this function reads a gpx file from https://waypointer.info/ and creats 
-#a 2d array with the name of the landmark at [x][0] and the lat and log at 
-#[x][1] [x][2] where x is a monument. there are x monuments totle
+
 def main():
     initData = readInInitData();
     points = readInGPSPoints();#not made yet
