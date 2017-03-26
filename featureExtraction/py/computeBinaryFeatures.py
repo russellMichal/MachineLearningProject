e5b7a63f0fbf418d1b7c701e409682d5441a2fac
rmichal2 <rmichal2@illinois.edu>
2017-03-26 16:12:31 -0500

    adding file export

--- featureExtraction/py/computeBinaryFeatures.py
+++ featureExtraction/py/computeBinaryFeatures.py
@@ -5,19 +5,26 @@ Created on Mon Mar  6 22:52:36 2017
 @author: rkm87
 """
 
+#this site will convert lat to a distance in feet/miles
+#http://www.somebits.com/weblog/tech/latitude-longitude-distance-approximations.html
+
 #the startingPoints is going to data read from micheals database, it will be in the format
 #lat, log, label
 #dataList is the list of features we have read in from readInInitData
 #bound is how far away a feature place can be and still get counted as close
 def computeBinaryFeatures(startingPoints, dataList,bound):
     retvec = []
+    
+    for i in range(0,len(startingPoints)):
+        retvec.append([])
         
-    for place in dataList:
-        if(startingPoints[0]>float(place[1])-bound and startingPoints[1]<float(place[2])+bound):
-            retvec.append(1)
-        else:
-            retvec.append(0)
-
-    retvec.append(startingPoints[len(startingPoints)-1])
+        for place in dataList:
+            print(place)
+            if(startingPoints[i][0]>float(place[1])-bound and startingPoints[i][1]<float(place[2])+bound):
+                retvec[i].append(1)
+            else:
+                retvec[i].append(0)
+    
+        retvec[i].append(startingPoints[i][len(startingPoints[i])-1])
 
     return retvec
--- featureExtraction/py/featureExtractor.py
+++ featureExtraction/py/featureExtractor.py
@@ -7,12 +7,20 @@ This is a temporary script file.
 from readInInitData import readInInitData
 from readInGPSPoints import readInGPSPoints
 from computeBinaryFeatures import computeBinaryFeatures
+from exporteToFile import exporteToFile
 
 def main():
     initData = readInInitData();
     points = readInGPSPoints();#not made yet
+    
+    #http://www.somebits.com/weblog/tech/latitude-longitude-distance-approximations.html
+    #.0001 ~ 36 feet
     featureVector = computeBinaryFeatures(points, initData,0.00010);
-    #exporteToFile();#not made yet dont know if we need this one
+    
+    for i in range(0,len(featureVector)):
+        print(featureVector[i])
+     
+    exporteToFile(featureVector);
                             
 
 main()
