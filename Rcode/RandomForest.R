2a2a8c0f441276b4c69478bc2026540fc209422f
taomo1003 <taomo1003@gmail.com>
2017-04-04 09:35:27 -0500

    Cross Validation

--- Rcode/RandomForest.R
+++ Rcode/RandomForest.R
@@ -10,3 +10,16 @@ fit <- randomForest(V9 ~ .,   data=data)
 print(fit) # view results 
 importance(fit) # importance of each predictor
 
+#Cross Validation
+require(caret)
+flds <- createFolds(data$V9, k = 10, list = TRUE, returnTrain = FALSE)
+
+for (i in 1:10){
+  testData <- data[flds[[i]], ]
+  trainData <- data[-flds[[i]],]
+  fit <- randomForest(V9 ~ .,   data=trainData)
+  pre <- predict(fit,testData)
+  print(confusionMatrix(pre,testData$V9))
+}
+
+
